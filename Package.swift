b490fa51d7a254bf8f2d99e76c7d553ead12e584
Emir Guven <106540353+guvenemirhan@users.noreply.github.com>
2025-07-22 17:59:15 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "GoogleMobileAdsMediationAppLovin", targets: ["GoogleMobileAdsMediationAppLovin"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git", exact: "13.0.0"),
+        .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git", from: "12.1.0"),
         .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.0.0")
     ],
     targets: [
@@ -18,12 +18,13 @@ let package = Package(
                 .product(name: "AppLovinSDK", package: "AppLovin-MAX-Swift-Package"),
                 .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
                 "AppLovinAdapter",
-            ]
+            ],
+            path: "Sources/GoogleMobileAdsMediationAppLovin"
         ),
         .binaryTarget(
             name: "AppLovinAdapter",
-            url: "https://github.com/YOUR-USERNAME/GoogleMobileAdsMediationAppLovin-SPM/releases/download/13.0.0.2/AppLovinAdapter.xcframework.zip",
-            checksum: "511c1bb793ec80b1baa22185df767e2f8d7ab72b81edc7a901923b26346e491e"
+            url: "https://github.com/guvenemirhan/GoogleMobileAdsMediationAppLovin-SPM/releases/download/13.0.0.2/AppLovinAdapter.xcframework.zip",
+            checksum: "GERÇEK-CHECKSUM-BURAYA"
         ),
     ]
 )
